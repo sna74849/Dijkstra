6a65c68c899b31bc6625994267715959fae37144
sna74849 <t.munekage@gmail.com>
2020-03-24 10:31:54 +0900

    Modify

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -6,7 +6,7 @@ public class Dijkstra {
 
 	private static final char[] CHAR_MAPPING = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 	
-	private static final Integer[][] vertexes_mapping = {{0,null,null,null,null,null,null,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
+	private static final Integer[][] VERTEXES = {{0,null,null,null,null,null,null,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
 							{null,0,null,null,null,null,null,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
 							{null,null,0,null,null,null,null,null,3,1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
 							{null,null,null,0,2,null,null,null,null,2,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null},
@@ -52,7 +52,7 @@ public class Dijkstra {
 
 		while(counter < 26) {
 			for(int i=0;i<26;i++) {
-				if(vertexes_mapping[number][i] != null && vertexes_mapping[number][i] != 0) {
+				if(VERTEXES[number][i] != null && VERTEXES[number][i] != 0) {
 					boolean flg = true;//座標を既に採用している。
 					for(int j=0;j<26;j++) {
 						if(sequence[j] != null) {
@@ -91,14 +91,14 @@ public class Dijkstra {
 		// 最短経路を探索
 		for(int i=0;i<26;i++) {
 			for(int j=0;j<26;j++) {		
-				if (vertexes_mapping[sequence[i]][sequence[j]] != null) {
+				if (VERTEXES[sequence[i]][sequence[j]] != null) {
 				// 対象ノードと隣接している
 					if(matrix[sequence[j]][1] != null) {
 					// この隣接ノードの経路はもう確立している
-						if(matrix[sequence[i]][1] > vertexes_mapping[sequence[i]][sequence[j]] + matrix[sequence[j]][1]) {
+						if(matrix[sequence[i]][1] > VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[j]][1]) {
 						// この隣接ノード経由の方がすでに確立している経路より対象ノードへの距離が短い
 							matrix[sequence[i]][0] = sequence[j];
-							matrix[sequence[i]][1] = vertexes_mapping[sequence[i]][sequence[j]] + matrix[sequence[j]][1];
+							matrix[sequence[i]][1] = VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[j]][1];
 						} else {
 						// この隣接ノード経由の方がすでに確立している経路より対象ノードへの距離が長いか同じ（処理なし）
 						}
@@ -107,11 +107,11 @@ public class Dijkstra {
 						if(matrix[sequence[i]][1] != null){
 						// 対象ノードの経路は既に確立している
 							matrix[sequence[j]][0] = sequence[i];
-							matrix[sequence[j]][1] = vertexes_mapping[sequence[i]][sequence[j]] + matrix[sequence[i]][1];
+							matrix[sequence[j]][1] = VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[i]][1];
 						} else {
 						// 対象ノードの経路はまだ確立していない
 							matrix[sequence[j]][0] = sequence[i];
-							matrix[sequence[j]][1] = vertexes_mapping[sequence[i]][sequence[j]];
+							matrix[sequence[j]][1] = VERTEXES[sequence[i]][sequence[j]];
 						}
 					}
 				}
