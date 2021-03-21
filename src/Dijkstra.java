680851e241b00d6a1cc49faaa73e65aa45d2d0f2
sna74849 <t.munekage@gmail.com>
2021-03-21 16:12:21 +0900

    Showing only total cost

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -44,7 +44,7 @@ public class Dijkstra {
 	static Integer[] sequence = new Integer[26];
 
 	/**
-	 * 各バーテックスへ到達するまでのコストと隣接ノードを示す結果セットの二次元配列
+	 * 各バーテックスへ到達するまでのコストと隣接バーテックスを示す結果セットの二次元配列
 	 */
 	static Integer[][] matrix = new Integer[26][2];
 
@@ -64,17 +64,18 @@ public class Dijkstra {
 	public static void main(String[] args) {
 
 		// 経路を確立するために並び替え
-		sequence[0] = 0;
+		sequence[0] = 8;
 		int number = sequence[0];
 
 		while(counter < 26) {
 			for(int i=0;i<26;i++) {
 				if(VERTEXES[number][i] != null && VERTEXES[number][i] != 0) {
-					boolean flg = true;//座標を既に採用している。
+					boolean flg = true;//座標をまだ採用していない
 					for(int j=0;j<26;j++) {
 						if(sequence[j] != null) {
 							if(sequence[j] == i) {
-								flg = false;//座標をまだ採用していない。
+								flg = false;//座標を既に採用している
+								break;
 							}
 						}
 					}		
@@ -88,14 +89,15 @@ public class Dijkstra {
 			// 孤立ルートを代入する
 			if(sequence[current] == null) {
 				for(int i=0;i<26;i++) {
-					boolean flg = true;//座標を既に採用している。
+					boolean flg = true;//座標をまだ採用していない
 					for(int j=0;j<26;j++) {
 						if(sequence[j] != null) {
 							if(sequence[j] == i) {
-								flg = false;//座標をまだ採用していない。
+								flg = false;//座標を既に採用している
+								break;
 							}
 						}
-					}		
+					}
 					if(flg) {
 						sequence[counter] = i;
 						counter++;
@@ -105,36 +107,56 @@ public class Dijkstra {
 			number = sequence[current];
 		}
 
-		// 最短経路を探索
-		for(int i=0;i<26;i++) {
-			for(int j=0;j<26;j++) {		
-				if (VERTEXES[sequence[i]][sequence[j]] != null) {
-				// 対象ノードと隣接している
-					if(matrix[sequence[j]][1] != null) {
-					// この隣接ノードの経路はもう確立している
-						if(matrix[sequence[i]][1] > VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[j]][1]) {
-						// この隣接ノード経由の方がすでに確立している経路より対象ノードへの距離が短い
-							matrix[sequence[i]][0] = sequence[j];
-							matrix[sequence[i]][1] = VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[j]][1];
+		// 孤立ルートからスタートかどうかを判断
+		if (current == 1) {
+			// 最初で終わっていれば孤立ルートとみなして全て０を代入する
+			for(int i=0;i<26;i++){
+				matrix[sequence[i]][0] = sequence[i];
+				matrix[sequence[i]][1] = 0;
+			}
+		} else {
+			// 最短経路を探索
+			for(int i=0;i<26;i++) {
+				for(int j=0;j<26;j++) {
+					if (VERTEXES[sequence[i]][sequence[j]] != null) {
+						// 対象バーテックスと隣接している
+						if(matrix[sequence[j]][1] != null) {
+							// この隣接バーテックスの経路はもう確立している
+							if(matrix[sequence[i]][1] > VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[j]][1]) {
+								// この隣接バーテックス経由の方がすでに確立している経路より対象バーテックスへの距離が短い
+								matrix[sequence[i]][0] = sequence[j];
+								matrix[sequence[i]][1] = VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[j]][1];
+								// 確立した経路により隣接バーテックスの経路が更に短くなるか遡及する
+								for(int k=0;k<26;k++) {
+									if(VERTEXES[sequence[i]][k] != null && matrix[k][1] != null) {
+										// 対象バーテックスと隣接するバーテックスの経路がすでに確立している
+										if(matrix[k][1] > VERTEXES[sequence[i]][k] + matrix[sequence[i]][1]) {
+											// この隣接バーテックスと確立したルートより対象バーテックスと隣接するバーテックスの距離も短くなる
+											matrix[k][0] = sequence[i];
+											matrix[k][1] = VERTEXES[sequence[i]][k] + matrix[sequence[i]][1];
+										}
+									}
+								}
+							} else {
+								// この隣接バーテックス経由の方がすでに確立している経路より対象バーテックスへの距離が長いか同じ（処理なし）
+							}
 						} else {
-						// この隣接ノード経由の方がすでに確立している経路より対象ノードへの距離が長いか同じ（処理なし）
-						}
-					} else {
-					// この隣接ノードの経路はまだ確立していない
-						if(matrix[sequence[i]][1] != null){
-						// 対象ノードの経路は既に確立している
-							matrix[sequence[j]][0] = sequence[i];
-							matrix[sequence[j]][1] = VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[i]][1];
-						} else {
-						// 対象ノードの経路はまだ確立していない
-							matrix[sequence[j]][0] = sequence[i];
-							matrix[sequence[j]][1] = VERTEXES[sequence[i]][sequence[j]];
+							// この隣接バーテックスの経路はまだ確立していない
+							if(matrix[sequence[i]][1] != null){
+								// 対象バーテックスの経路は既に確立している
+								matrix[sequence[j]][0] = sequence[i];
+								matrix[sequence[j]][1] = VERTEXES[sequence[i]][sequence[j]] + matrix[sequence[i]][1];
+							} else {
+								// 対象バーテックスの経路はまだ確立していない
+								matrix[sequence[j]][0] = sequence[i];
+								matrix[sequence[j]][1] = VERTEXES[sequence[i]][sequence[j]];
+							}
 						}
 					}
 				}
 			}
 		}
 		// 最短経路のコストを表示
-		System.out.println("Total:" + matrix[25][1]);
+		System.out.println("Total:" + matrix[13][1]);
 	}
 }
